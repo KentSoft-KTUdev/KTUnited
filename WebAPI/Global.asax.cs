59425d85b91d3b34256cbf7a4ef0f2f369c4e629
Edvardas Alaburda <edvardas.alaburda@ktu.edu>
2018-03-11 16:01:44 +0200

    +JSON loops handling

--- WebAPI/Global.asax.cs
+++ WebAPI/Global.asax.cs
@@ -18,6 +18,8 @@ namespace WebAPI
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            HttpConfiguration httpConfiguration = GlobalConfiguration.Configuration;
+            httpConfiguration.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
         }
     }
 }
