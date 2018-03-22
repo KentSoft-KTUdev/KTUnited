d0f43842ec10cf9cab483c2217ce1bb38c6bfdb2
Edvardas Alaburda <edvardas.alaburda@ktu.edu>
2018-03-22 14:07:29 +0200

    +created base actions controller for Resident and Views

---
+++ WebAPI/Controllers/ResidentController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAPI.Controllers
+{
+    public class ResidentController : Controller
+    {
+        // GET: Resident
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // sukurkit resident forma (ji bus naudojama sukurimui residento arba modifikacijai)
+        public ActionResult ResidentForm()
+        {
+            return View();
+        }
+
+        // sukurkit svecio registravimo forma is residento puses
+        public ActionResult RegisterGuest()
+        {
+            return View();
+        }
+
+        // sukurkit kazka kas butu kaip to gyventojo vizitu sarasas
+        public ActionResult Visits()
+        {
+            return View();
+        }
+    }
+}
