fefbb648ba2bb207ed36785b04977779a8154872
DESKTOP-1CNF0H0\Dalius <dalius.zukauskas@gmail.com>
2018-03-28 19:06:23 +0300

    Drop down lists populated with data

--- WebAPI/Controllers/ResidentController.cs
+++ WebAPI/Controllers/ResidentController.cs
@@ -19,10 +19,44 @@ namespace WebAPI.Controllers
         // sukurkit resident forma (ji bus naudojama sukurimui residento arba modifikacijai)
         public ActionResult ResidentForm()
         {
+            #region ViewBag
             RoomRepository roomRepository = new RoomRepository();
             List<Room> rooms = roomRepository.GetAll();
             DormitoryRepository dormitoryRepository = new DormitoryRepository();
             List<Dormitory> dormitories = dormitoryRepository.GetAll();
+
+            List<SelectListItem> Dormitory = new List<SelectListItem>();
+            if (dormitories.Count() == 0)
+            {
+                Dormitory.Add(new SelectListItem { Text = "Nėra registruotų bendrabučių", Value = "1" });
+            }
+            else
+            {
+                for (int i = 0; i < dormitories.Count(); i++)
+                {
+                    var temp = new SelectListItem { Text = dormitories[i].Name, Value = (i + 1).ToString() };
+                    Dormitory.Add(temp);
+
+                }
+            }
+            ViewBag.Dormitory = Dormitory;
+
+            List<SelectListItem> Rooms = new List<SelectListItem>();
+            if (rooms.Count() == 0)
+            {
+                Rooms.Add(new SelectListItem { Text = "Nėra registruotų kambarių", Value = "1" });
+            }
+            else
+            {
+                for (int i = 0; i < rooms.Count(); i++)
+                {
+                    var temp = new SelectListItem { Text = rooms[i].Number.ToString(), Value = (i + 1).ToString() };
+                    Rooms.Add(temp);
+
+                }
+            }
+            ViewBag.Rooms = Rooms;
+            #endregion
             return View();
         }
 
