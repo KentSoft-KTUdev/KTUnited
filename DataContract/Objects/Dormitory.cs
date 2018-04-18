dfde31a290b6b01254a860f478dae760312e4e6a
Edvardas Arnoldas Alaburda <edvardas.alaburda@ktu.edu>
2018-04-18 18:31:19 +0300

    +client side refactoring +client api update

--- DataContract/Objects/Dormitory.cs
+++ DataContract/Objects/Dormitory.cs
@@ -11,10 +11,5 @@ namespace DataContract.Objects
         public int ID { get; set; }
         public string Name { get; set; }
         public string Adress { get; set; }
-        public List<Resident> Residents { get; set; }
-        public List<Guard> Guard { get; set; }
-        public Administrator Administrator { get; set; }
-        public List<Visit> Visits { get; set; }
-        public List<Room> Rooms { get; set; }
     }
 }
