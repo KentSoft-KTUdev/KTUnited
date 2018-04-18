2415ee4ae419e8ba00ce1cf8c0e028fcfa2b5907
Visagalis <juliuszs@gmail.com>
2018-04-18 20:42:11 +0300

    Dormitory mappers and proper usage in DormitoryController.

---
+++ WebAPI/Extensions/Mappers.cs
@@ -0,0 +1,36 @@
+﻿using DormitoryContract = DataContract.Objects.Dormitory;
+using DormitoryInternal = WebAPI.DataModels.Dormitory;
+
+namespace WebAPI.Extensions
+{
+    public static class Mappers
+    {
+        public static DormitoryContract ToContract(this DormitoryInternal entity)
+        {
+            if (entity == null)
+                return null;
+
+            var dorm = new DormitoryContract
+            {
+                ID = entity.ID,
+                Adress = entity.Adress,
+                Name = entity.Name
+            };
+            return dorm;
+        }
+
+        public static DormitoryInternal ToInternal(this DormitoryContract entity)
+        {
+            if (entity == null)
+                return null;
+
+            var dorm = new DormitoryInternal
+            {
+                ID = entity.ID,
+                Adress = entity.Adress,
+                Name = entity.Name
+            };
+            return dorm;
+        }
+    }
+}
