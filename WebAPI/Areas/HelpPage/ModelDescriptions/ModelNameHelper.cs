03979025b4d0b6110e72f76c17c489a2486c15a0
Visagalis <juliuszs@gmail.com>
2018-04-18 21:18:30 +0300

    Workaround for API help page errors.
    https://www.c-sharpcorner.com/UploadFile/d132a2/workaround-in-Asp-Net-webapi-help-page/

--- WebAPI/Areas/HelpPage/ModelDescriptions/ModelNameHelper.cs
+++ WebAPI/Areas/HelpPage/ModelDescriptions/ModelNameHelper.cs
@@ -7,7 +7,7 @@ namespace WebAPI.Areas.HelpPage.ModelDescriptions
 {
     internal static class ModelNameHelper
     {
-        // Modify this to provide custom model name mapping.
+        // Modify this to provide custom model name mapping.  
         public static string GetModelName(Type type)
         {
             ModelNameAttribute modelNameAttribute = type.GetCustomAttribute<ModelNameAttribute>();
@@ -16,18 +16,19 @@ namespace WebAPI.Areas.HelpPage.ModelDescriptions
                 return modelNameAttribute.Name;
             }
 
-            string modelName = type.Name;
+            string modelName = type.FullName;
             if (type.IsGenericType)
             {
-                // Format the generic type name to something like: GenericOfAgurment1AndArgument2
+                // Format the generic type name to something like: GenericOfAgurment1AndArgument2  
                 Type genericType = type.GetGenericTypeDefinition();
                 Type[] genericArguments = type.GetGenericArguments();
-                string genericTypeName = genericType.Name;
+                string genericTypeName = genericType.FullName;
 
-                // Trim the generic parameter counts from the name
+                // Trim the generic parameter counts from the name  
                 genericTypeName = genericTypeName.Substring(0, genericTypeName.IndexOf('`'));
                 string[] argumentTypeNames = genericArguments.Select(t => GetModelName(t)).ToArray();
-                modelName = String.Format(CultureInfo.InvariantCulture, "{0}Of{1}", genericTypeName, String.Join("And", argumentTypeNames));
+                modelName = String.Format(CultureInfo.InvariantCulture, "{0}Of{1}", genericTypeName,
+                    String.Join("And", argumentTypeNames));
             }
 
             return modelName;
