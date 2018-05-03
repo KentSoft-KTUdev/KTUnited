5a9b5baf91bbf4290335bd2deaea80f3ffacae5a
DESKTOP-1CNF0H0\Dalius <dalius.zukauskas@gmail.com>
2018-05-03 03:06:58 +0300

    made razor forms to create objects and created visits page for residents | needs fixing with lists because creating objects require attributes (dormitory and rooms) as objects

--- WebAPI/Controllers/AdminController.cs
+++ WebAPI/Controllers/AdminController.cs
@@ -81,8 +81,61 @@ namespace WebAPI.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult CreateGuard([Bind(Include = "PersonalCode,Adress")] Dormitory dormitory)
+        public ActionResult CreateGuard([Bind(Include = "PersonalCode,Name,Surname,Dormitory")] Guard guard)
         {
+            //DormitoryRepository dormitoryRepository = new DormitoryRepository();
+            //List<Dormitory> dormitories = dormitoryRepository.GetAll();
+            //for (int i = 0; i < dormitories.Count(); i++)
+            //{
+            //    if (dormitories[i].Name == guard.Dormitory.Name)
+            //    {
+            //        // add here, but the guard dormitory value is null because it only obtains string from web ?
+            //    }
+
+            //}
+            GuardRepository guardRepository = new GuardRepository();
+            guardRepository.Create((guard));
+            return RedirectToAction("Index");
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CreateRoom([Bind(Include = "Dormitory,Number")] Room room)
+        {
+            //DormitoryRepository dormitoryRepository = new DormitoryRepository();
+            //List<Dormitory> dormitories = dormitoryRepository.GetAll();
+            //for (int i = 0; i < dormitories.Count(); i++)
+            //{
+            //    if (dormitories[i].Name == room.Dormitory.Name)
+            //    {
+            //        // add here, but the guard dormitory value is null because it only obtains string from web ?
+            //    }
+
+            //}
+            RoomRepository roomRepository = new RoomRepository();
+            roomRepository.Create((room));
+            return RedirectToAction("Index");
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CreateResident([Bind(Include = "Name,Surname,PersonalCode,Room,Dormitory")] Resident resident)
+        {
+            //DormitoryRepository dormitoryRepository = new DormitoryRepository();
+            //List<Dormitory> dormitories = dormitoryRepository.GetAll();
+            //for (int i = 0; i < dormitories.Count(); i++)
+            //{
+            //    if (dormitories[i].Name == guard.Dormitory.Name)
+            //    {
+            //        // add here, but the guard dormitory value is null because it only obtains string from web ?
+                      // same with rooms
+            //    }
+
+            //}
+            ResidentRepository residentRepository = new ResidentRepository();
+            residentRepository.Create((resident));
             return RedirectToAction("Index");
 
         }
@@ -112,11 +165,49 @@ namespace WebAPI.Controllers
             }
             ViewBag.Dormitory = Dormitory;
             #endregion
+            RoomRepository rooms = new RoomRepository();
+            List<Room> roomList = rooms.GetAll();
+            if (roomList.Count() == 0)
+            {
+                Console.Write("Liudna");
+            }
+
+
             return View();
         }
 
         public ActionResult RegisterGuard()
         {
+            #region ViewBag
+            DormitoryRepository dormitoryRepository = new DormitoryRepository();
+            List<Dormitory> dormitories = dormitoryRepository.GetAll();
+
+
+            List<SelectListItem> Dormitory = new List<SelectListItem>();
+            if (dormitories.Count() == 0)
+            {
+                Dormitory.Add(new SelectListItem { Text = "Nėra registruotų bendrabučių", Value = "1" });
+            }
+            else
+            {
+                Dormitory.Add(new SelectListItem { Text = "", Value = "1" });
+                for (int i = 0; i < dormitories.Count(); i++)
+                {
+                    var temp = new SelectListItem { Text = dormitories[i].Name, Value = (i + 2).ToString() };
+                    Dormitory.Add(temp);
+
+                }
+            }
+            ViewBag.Dormitory = Dormitory;
+            #endregion
+            GuardRepository guards = new GuardRepository();
+            List<Guard> guardList = guards.GetAll();
+            if (guardList.Count() == 0)
+            {
+                Console.Write("Liudna");
+            }
+
+
             return View();
 
         }
