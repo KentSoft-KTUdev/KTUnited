99057df23712a75a4878a7c91694be3ee3bd8193
Edvardas Arnoldas Alaburda <edvardas.alaburda@ktu.edu>
2018-05-07 05:28:20 +0300

    +reworked MVC elements to have proper implementation and functionality

--- WebAPI/Controllers/AdminController.cs
+++ WebAPI/Controllers/AdminController.cs
@@ -14,69 +14,80 @@ namespace WebAPI.Controllers
         private AdministratorRepository administratorRepository = new AdministratorRepository();
         private DormitoryRepository dormitoryRepository = new DormitoryRepository();
         private GuardRepository guardRepository = new GuardRepository();
+        private RoomRepository roomRepository = new RoomRepository();
+        private ResidentRepository residentRepository = new ResidentRepository();
+
         // GET: Admin
         public ActionResult Index(Administrator administrator)
         {
             return View(administrator);
         }
 
-        public ActionResult ResidentForm()
+        public ActionResult RegisterResident()
         {
             #region ViewBag
-            RoomRepository roomRepository = new RoomRepository();
-            List<Room> rooms = roomRepository.GetAll();
-            DormitoryRepository dormitoryRepository = new DormitoryRepository();
-            List<Dormitory> dormitories = dormitoryRepository.GetAll();
-
-
-            List<SelectListItem> Dormitory = new List<SelectListItem>();
-            if (dormitories.Count() == 0)
+            if (Session["Username"] != null)
             {
-                Dormitory.Add(new SelectListItem { Text = "Nėra registruotų bendrabučių", Value = "1" });
-            }
-            else
-            {
-                Dormitory.Add(new SelectListItem { Text = "", Value = "1" });
-                for (int i = 0; i < dormitories.Count(); i++)
+                List<Dormitory> dormitories = dormitoryRepository.GetAll();
+                List<SelectListItem> Dormitory = new List<SelectListItem>();
+                if (dormitories.Count == 0)
                 {
-                    var temp = new SelectListItem { Text = dormitories[i].Name, Value = (i + 2).ToString() };
-                    Dormitory.Add(temp);
-
+                    Dormitory.Add(new SelectListItem { Text = "Nėra registruotų bendrabučių", Value = "-1" });
                 }
-            }
-            ViewBag.Dormitory = Dormitory;
-
-            List<SelectListItem> Rooms = new List<SelectListItem>();
-            if (rooms.Count() == 0)
-            {
-                Rooms.Add(new SelectListItem { Text = "Nėra registruotų kambarių", Value = "1" });
-            }
-            else
-            {
-                Rooms.Add(new SelectListItem { Text = "", Value = "1" });
-                for (int i = 0; i < rooms.Count(); i++)
+                else
                 {
-                    var temp = new SelectListItem { Text = rooms[i].Number.ToString(), Value = (i + 2).ToString() };
-                    Rooms.Add(temp);
+                    Dormitory.Add(new SelectListItem { Text = "", Value = "-1" });
+                    for (int i = 0; i < dormitories.Count(); i++)
+                    {
+                        var temp = new SelectListItem { Text = dormitories[i].Name, Value = dormitories[i].ID.ToString() };
+                        Dormitory.Add(temp);
 
+                    }
                 }
+                ViewBag.DormitoryId = Dormitory;
+                List<Room> rooms = roomRepository.GetAll();
+                List<SelectListItem> Rooms = new List<SelectListItem>();
+                if (rooms.Count == 0)
+                {
+                    Rooms.Add(new SelectListItem { Text = "Nėra registruotų kambarių", Value = "-1" });
+                }
+                else
+                {
+                    Rooms.Add(new SelectListItem { Text = "", Value = "-1" });
+                    for (int i = 0; i < rooms.Count; i++)
+                    {
+                        var temp = new SelectListItem { Text = rooms[i].Number.ToString(), Value = rooms[i].ID.ToString() };
+                        Rooms.Add(temp);
+                    }
+                }
+                ViewBag.RoomId = Rooms;
+                ViewBag.DefaultPassword = GetRandomAlphaNumeric();
+                #endregion
+                return View();
             }
-            ViewBag.Rooms = Rooms;
-            #endregion
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult RegisterResident([Bind(Include = "Name,Surname,PersonalCode,RoomId,DormitoryId,Password,Username")] Resident resident)
+        {
+            ViewBag.RoomId = new SelectList(dormitoryRepository.GetAll(), "ID", "Name", resident.RoomId);
+            ViewBag.DormitoryId = new SelectList(dormitoryRepository.GetAll(), "ID", "Name", resident.DormitoryId);
+            ViewBag.DefaultPassword = resident.Password;
+            residentRepository.Create((resident));
             return View();
+
         }
 
 
-        public ActionResult RegisterDormitory()
+        public ActionResult CreateDormitory()
         {
             if (Session["Username"] != null)
             {
                 return View();
             }
-            else
-            {
-                return RedirectToAction("Index");
-            }
+            return RedirectToAction("Index");
         }
 
         [HttpPost]
@@ -84,101 +95,20 @@ namespace WebAPI.Controllers
         public ActionResult CreateDormitory([Bind(Include = "Name,Adress")] Dormitory dormitory)
         {
             dormitoryRepository.Create((dormitory));
-            return RedirectToAction("Index");
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult CreateGuard([Bind(Include = "PersonalCode,Name,Surname,DormitoryId,Username,Password")] Guard guard)
-        {
-            if(guardRepository.Create((guard)).IsSuccessStatusCode)
-            {
-                return View();
-            }
-            else
-            {
-                return View("RegisterGuard", guard);
-            } 
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult CreateRoom([Bind(Include = "Dormitory,Number")] Room room)
-        {
-            //DormitoryRepository dormitoryRepository = new DormitoryRepository();
-            //List<Dormitory> dormitories = dormitoryRepository.GetAll();
-            //for (int i = 0; i < dormitories.Count(); i++)
-            //{
-            //    if (dormitories[i].Name == room.Dormitory.Name)
-            //    {
-            //        // add here, but the guard dormitory value is null because it only obtains string from web ?
-            //    }
-
-            //}
-            RoomRepository roomRepository = new RoomRepository();
-            roomRepository.Create((room));
-            return RedirectToAction("Index");
-
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult CreateResident([Bind(Include = "Name,Surname,PersonalCode,Room,Dormitory")] Resident resident)
-        {
-            //DormitoryRepository dormitoryRepository = new DormitoryRepository();
-            //List<Dormitory> dormitories = dormitoryRepository.GetAll();
-            //for (int i = 0; i < dormitories.Count(); i++)
-            //{
-            //    if (dormitories[i].Name == guard.Dormitory.Name)
-            //    {
-            //        // add here, but the guard dormitory value is null because it only obtains string from web ?
-                      // same with rooms
-            //    }
-
-            //}
-            ResidentRepository residentRepository = new ResidentRepository();
-            residentRepository.Create((resident));
-            return RedirectToAction("Index");
-
-        }
-
-
-        public ActionResult RegisterRoom()
-        {
-            #region ViewBag
-            DormitoryRepository dormitoryRepository = new DormitoryRepository();
-            List<Dormitory> dormitories = dormitoryRepository.GetAll();
-
-
-            List<SelectListItem> Dormitory = new List<SelectListItem>();
-            if (dormitories.Count() == 0)
-            {
-                Dormitory.Add(new SelectListItem { Text = "Nėra registruotų bendrabučių", Value = "1" });
-            }
-            else
-            {
-                Dormitory.Add(new SelectListItem { Text = "", Value = "1" });
-                for (int i = 0; i < dormitories.Count(); i++)
-                {
-                    var temp = new SelectListItem { Text = dormitories[i].Name, Value = (i + 2).ToString() };
-                    Dormitory.Add(temp);
-
-                }
-            }
-            ViewBag.Dormitory = Dormitory;
-            #endregion
-            RoomRepository rooms = new RoomRepository();
-            List<Room> roomList = rooms.GetAll();
-            if (roomList.Count() == 0)
-            {
-                Console.Write("Liudna");
-            }
-
-
             return View();
         }
 
-        public ActionResult RegisterGuard(Guard guard)
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult RegisterGuard([Bind(Include = "PersonalCode,Name,Surname,DormitoryId,Username,Password")] Guard guard)
+        {
+            ViewBag.DormitoryId = new SelectList(dormitoryRepository.GetAll(), "ID", "Name", guard.DormitoryId);
+            ViewBag.DefaultPassword = guard.Password;
+            guardRepository.Create(guard);
+            return View();
+        }
+
+        public ActionResult RegisterGuard()
         {
             if (Session["Username"] != null)
             {
@@ -199,15 +129,47 @@ namespace WebAPI.Controllers
                         Dormitory.Add(temp);
                     }
                 }
-                ViewBag.Dormitory = Dormitory;
+                ViewBag.DormitoryId = Dormitory;
                 ViewBag.DefaultPassword = GetRandomAlphaNumeric();
                 #endregion
                 return View();
             }
-            else
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CreateRoom([Bind(Include = "DormitoryId,Number")] Room room)
+        {
+            ViewBag.DormitoryId = new SelectList(dormitoryRepository.GetAll(), "ID", "Name", room.DormitoryId);
+            return View();
+        }
+
+        public ActionResult CreateRoom()
+        {
+            if (Session["Username"] != null)
             {
-                return RedirectToAction("Index");
+                #region ViewBag
+                List<Dormitory> dormitories = dormitoryRepository.GetAll();
+                List<SelectListItem> Dormitory = new List<SelectListItem>();
+                if (dormitories.Count() == 0)
+                {
+                    Dormitory.Add(new SelectListItem { Text = "Nėra registruotų bendrabučių", Value = "-1" });
+                }
+                else
+                {
+                    Dormitory.Add(new SelectListItem { Text = "", Value = "-1" });
+                    for (int i = 0; i < dormitories.Count(); i++)
+                    {
+                        var temp = new SelectListItem { Text = dormitories[i].Name, Value = dormitories[i].ID.ToString() };
+                        Dormitory.Add(temp);
+                    }
+                }
+                ViewBag.DormitoryId = Dormitory;
+                #endregion
+                return View();
             }
+            return RedirectToAction("Index");
         }
 
         public ActionResult Login()
@@ -233,10 +195,7 @@ namespace WebAPI.Controllers
             {
                 return View();
             }
-            else
-            {
-                return RedirectToAction("Index");
-            }
+            return RedirectToAction("Index");
         }
 
         private string GetRandomAlphaNumeric()
